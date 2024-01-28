5b4f57804893a1dfdb435fafa0ee9015cfdfbb6b
Irtaza Khalid <Irtazakhalidll@gmail.com>
2024-01-28 13:56:41 +0500

    Its main door class

---
+++ Door.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+
+using namespace std;
+
+
+
+class Door {
+
+private:
+
+bool isopen;
+
+bool isLocked;
+
+public:
+
+Door(); // Initializes the door as flosed and void openDoor();
+
+
+void openDoor();
+void closeDoor();
+
+void lockDoor();
+
+void unlock();
+
+bool isDoorOpen() const;
+bool isDoorLocked() const;
+
+
+
+
+bool isDoorLocked() const;
+};
+
+int main(){ 
+
+return 0;
+}
